6a57c3ca1f3b436c13eb8bdec25c4430b5fcdfbd
peterpike01 <peter.hecht@peterpike.de>
2026-05-31 16:01:55 +0200

    erster versuch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.camera:camera-camera2:${camerax_version}")
     implementation("androidx.camera:camera-lifecycle:${camerax_version}")
     implementation("androidx.camera:camera-video:${camerax_version}")
-    implementation("androidx.camera:camera-view:${camerax_version}")
+ //   implementation("androidx.camera:camera-view:${camerax_version}")
     implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
     implementation("androidx.camera:camera-extensions:${camerax_version}")
 
